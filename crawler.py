0b1d92dbe5f3327be4df6f58eda7f1c91a9ec285
carsonzhu <carsonzhu@tencent.com>
2023-06-30 17:48:35 +0800

    add crawler.py

---
+++ crawler.py
@@ -0,0 +1,99 @@
+import os
+import re
+import sys
+import time
+import zipfile
+from shutil import copytree, rmtree
+from urllib.request import urlretrieve
+
+from selenium import webdriver
+
+URL = "http://huma.ysepan.com"
+ZIP_FILE = 'huma.zip'
+
+try:
+    os.unlink(ZIP_FILE)
+except Exception:
+    pass
+
+os.environ['MOZ_HEADLESS'] = '1'
+browser = webdriver.Firefox() # type: ignore
+
+timeout = 10
+
+browser.get(URL)
+i = 0
+while True:
+    try:
+        menu_list = browser.find_element(by='id', value='menuList')
+        break
+    except Exception as e:
+        if i > timeout:
+            print("failed to find menuList", repr(e))
+            exit(1)
+        time.sleep(1)
+        i += 1
+
+while True:
+    items = menu_list.find_elements(by='tag name', value='li')
+    if len(items) > 1:
+        break
+    time.sleep(1)
+
+target = None
+for item in items:
+    a = item.find_element(by='tag name', value='a')
+    if a.text.startswith("05 虎码 鼠须管 iRime"):
+        target = item
+        a.click()
+        break
+
+if target is None:
+    print("failed to find menu entry")
+    exit(1)
+
+i = 0
+while True:
+    try:
+        ul = target.find_element(by='tag name', value='ul')
+        break
+    except Exception as e:
+        if i > timeout:
+            print("failed to get sublist after timeout")
+            exit(1)
+        time.sleep(1)
+        i += 1
+
+url = None
+name = None
+items = ul.find_elements(by='tag name', value='li')
+for item in items:
+    if item.text.startswith('虎码秃版 鼠须管'):
+        a = item.find_element(by='tag name', value='a')
+        url = a.get_attribute('href')
+        name = item.text
+        break
+
+if url is None:
+    print("can't find zip file entry")
+    exit(1)
+
+browser.close()
+assert name is not None
+m = re.match(r'.*(?P<file>\d{4}\.\d{2}\.\d{2}\.zip)', name)
+if m is None:
+    print(f"failed to extract filename: {name}")
+    exit(1)
+date = m.group('file')
+
+print(f'v{date}')
+print(f'downloading {name}', file=sys.stderr)
+
+urlretrieve(url, ZIP_FILE)
+with zipfile.ZipFile(ZIP_FILE, 'r', metadata_encoding='cp936') as zip_ref:
+    folder = zip_ref.filelist[0].filename[:-1]
+    zip_ref.extractall('.')
+    copytree(folder, '.', dirs_exist_ok=True)
+
+os.unlink(ZIP_FILE)
+rmtree(folder)
